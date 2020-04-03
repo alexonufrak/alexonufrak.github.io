4d8059000054f09c363bb96fc4dfa0ac944aaaab
alexonufrak <60350877+alexonufrak@users.noreply.github.com>
2020-04-02 20:24:51 -0400

    fix

--- inst377/A2/main.js
+++ inst377/A2/main.js
@@ -21,16 +21,14 @@ for(let i = 1; i <= 5; i++) {
 /* Wiring up the Darken/Lighten button */
 
 btn.onclick = function() {
-    const bclass = btn.getAttribute('class');
-    if(bclass === 'dark') {
-      btn.setAttribute('class','light');
-      btn.textContent = 'Lighten';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-    } 
-    else {
-      btn.setAttribute('class','dark');
-      btn.textContent = 'Darken';
-      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-    }
+  const btnClass = btn.getAttribute('class');
+  if(btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
-  
+}
